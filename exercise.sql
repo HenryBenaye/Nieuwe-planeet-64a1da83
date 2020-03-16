06335fcca3dc30a607e5b5c0fbf5846fe22cc56e
HENRY BENAYE <2106747@talnet.nl>
2020-03-16 12:54:28 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,5 +1,15 @@
-update planeten
-SET bezoek_datum = NOW();
+
+alter table planeten
+add column id int NOT NULL auto_increment primary key;
+insert into planeten (naam, diameter, afstand_tot_de_zon, massa) values("Mars", 6794, 227936640, 0.1)
+
+
+
+
+
+
+
+
 
 
 
